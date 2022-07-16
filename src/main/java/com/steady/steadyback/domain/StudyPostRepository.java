88d58d313ac107ec478c76ff642e584bccebf9ce
변지은 <bje5774@gmail.com>
2022-07-17 00:25:21 +0900

    Fix : StudyPostRepository 수정

--- src/main/java/com/steady/steadyback/domain/StudyPostRepository.java
+++ src/main/java/com/steady/steadyback/domain/StudyPostRepository.java
@@ -3,6 +3,10 @@ package com.steady.steadyback.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudyPostRepository extends JpaRepository<StudyPost, Long> {
+    public List<StudyPost> findAllByStudyId(Long studyId);
+
 }
