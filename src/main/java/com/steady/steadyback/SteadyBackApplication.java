713a4cdf3af9ae2e7dcd9d0c8fc82c10ee973b15
YOONJI KIM <90975718+yoonjiy@users.noreply.github.com>
2022-11-08 20:05:52 +0900

    Update SteadyBackApplication.java

--- src/main/java/com/steady/steadyback/SteadyBackApplication.java
+++ src/main/java/com/steady/steadyback/SteadyBackApplication.java
@@ -3,14 +3,12 @@ package com.steady.steadyback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.context.annotation.PropertySource;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @EnableScheduling
 @SpringBootApplication
-@PropertySource("classpath:/application-secret.properties")
 public class SteadyBackApplication {
 
     @PostConstruct
